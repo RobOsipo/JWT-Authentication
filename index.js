6d8c3a5a8e94e94a924ff607d60de0b30f352a1a
Robert <osipovitchr@gmail.com>
2022-03-20 11:02:48 -0400

    check + validationResults logic from express-validator

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const auth = require('./routes/auth')
 const app = express();
 
+app.use(express.json())
+
 app.use('/auth', auth)
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
+router.post('/signup', [
+    // ! This is how I use express validator to *check* if the user input passes my condition, besides isEmail There is isBool, isStrongPassword, etc...
+    check('email').isEmail(),
+    check('password').isLength({ min: 6 })
 
-router.get('/', (req, res) => {
+    // ! 
+], (req, res) => {
+    const { password, email} = req.body;
+    // ! Use validation results with req passed in
+    const errors = validationResult(req)
+// ! If the array of errors is not empty, (user input error), throw a 400 error msg with the errors sent to user
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    
+
     res.send('Auth Route Working')
 })
 
-modules.export = router;
+module.exports = router;
