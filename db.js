3df2de1d0db864aa0fab72db95a0f4ceb0d58e8b
Robert <osipovitchr@gmail.com>
2022-03-20 11:26:23 -0400

    mock DB + finish validating email

---
+++ db.js
@@ -0,0 +1,9 @@
+const users = [
+    {
+        email: "something@gmail.com",
+        password: 123456
+    }
+]
+
+
+module.exports = users;
--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,43 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
+const users = require('../db.js')
 
 router.post('/signup', [
     // ! This is how I use express validator to *check* if the user input passes my condition, besides isEmail There is isBool, isStrongPassword, etc...
-    check('email').isEmail(),
-    check('password').isLength({ min: 6 })
+    check('email', "Please provide a valid Email").isEmail(),
+    check('password', "Please provide a password greater than 5 characters").isLength({ min: 6 })
 
-    // ! 
+ 
 ], (req, res) => {
     const { password, email} = req.body;
+    // * validate the input from user
     // ! Use validation results with req passed in
     const errors = validationResult(req)
 // ! If the array of errors is not empty, (user input error), throw a 400 error msg with the errors sent to user
+
     if (!errors.isEmpty()) {
         return res.status(400).json({
             errors: errors.array()
         })
     }
 
+    // * validate if user does not already exist
+
+    let user = users.find((user) => {
+        return user.email === email
+    })
+
+    if (user) { res.status(400).json({
+        "errors": [
+            {
+                "msg": "This Email already exist"
+            }
+        ]
+    })}
     
 
-    res.send('Auth Route Working')
+    res.send('Validation Passed')
 })
 
 module.exports = router;
